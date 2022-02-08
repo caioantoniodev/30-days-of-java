d0f3ea9da660aab546d227e86afc5914e88ebb8b
Caio Roberto <caio.roberto@sensedia.com>
2022-02-08 17:09:09 -0300

    day 0

--- src/com/thirtydaysofthecode/day0/HelloWorld.java
+++ src/com/thirtydaysofthecode/day0/HelloWorld.java
@@ -2,7 +2,7 @@ package com.thirtydaysofthecode.day0;
 
 import java.util.Scanner;
 
-public class HelloWord {
+public class HelloWorld {
     public static void main(String[] args) {
         // Create a Scanner object to read input from stdin.
         Scanner scan = new Scanner(System.in);
