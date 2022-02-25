d1053e2e6bb642fefaa771a2d9c10fc7a6d717a2
Caio Roberto <caio.roberto@sensedia.com>
2022-02-25 11:03:45 -0300

    day 17

---
+++ src/com/thirtydaysofthejava/day17/Exceptions.java
@@ -0,0 +1,34 @@
+package com.thirtydaysofthejava.day17;
+
+import java.util.Scanner;
+
+public class Exceptions {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+
+            int n = in.nextInt();
+            int p = in.nextInt();
+            Calculator myCalculator = new Calculator();
+            try {
+                int ans = myCalculator.power(n, p);
+                System.out.println(ans);
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        in.close();
+    }
+}
+
+class Calculator {
+    public int power(int n, int p) throws Exception {
+        if (n < 0 || p < 0)
+            throw new Exception("n and p should be non-negative");
+
+        return (int) Math.pow(n, p);
+    }
+}
